390a32ae0d3c3a29619a19d56d5880d683ccc9de
onaralili <onerelili@gmail.com>
2020-07-22 23:11:37 +0400

    removed uninstall form

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 'use strict';
-chrome.runtime.setUninstallURL("https://goo.gl/forms/u4OmQXmfeDz4Urjt2");
 document.addEventListener('DOMContentLoaded', () => {
 
   var btInfo = document.getElementById("btinfo");
